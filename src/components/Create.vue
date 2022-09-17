094223ea13254630fb60f5c8a3da2a8a33b8b524
Christina Petit <petitpois24@gmail.com>
2022-09-17 14:27:27 +0200

    cleaning up the add job component

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,14 +1,16 @@
 <script setup>
-import { ref } from "@vue/reactivity"; // import ref  from vue
+import { reactive } from "@vue/reactivity"; // import ref  from vue
 import { useMutation } from "@urql/vue";
 /*  Create field for job form  - corresponds to the fields we have in our Webiny CMS */
 
-const title = ref("");
-const desc = ref("");
-const jobUrl = ref("");
-const location = ref("");
-const contact = ref("");
-const expires = ref("");
+const newJob = reactive({
+  title: "",
+  desc: "",
+  jobUrl: "",
+  location: "",
+  contact: "",
+  expires: ""
+})
 const createJobResult = useMutation(`
 mutation(
   $title:String!, 
@@ -42,14 +44,14 @@ createJob(data:{
 /* * This function will take all the input from the form and push it into an object.
 Then it will push the object to the jobs array and reset all the input fields.
 */
-const checkFields = () => {
+const checkFields = (newJob) => {
   if (
-    title.value == "" ||
-    desc.value == "" ||
-    contact.value == "" ||
-    jobUrl.value == "" ||
-    location.value == "" ||
-    expires.value == ""
+    newJob.title == "" ||
+    newJob.desc == "" ||
+    newJob.contact == "" ||
+    newJob.jobUrl == "" ||
+    newJob.location == "" ||
+    newJob.expires == ""
   ) {
     alert("Fill in the fields");
   } else {
@@ -58,12 +60,12 @@ const checkFields = () => {
 };
 const addJob = () => {
   const variables = {
-    title: title.value,
-    contact: contact.value,
-    desc: desc.value,
-    location: location.value,
-    url: jobUrl.value,
-    expiry: new Date(expires.value).toISOString().split("T")[0],
+    title: newJob.title,
+    contact: newJob.contact,
+    desc: newJob.desc,
+    location: newJob.location,
+    url: newJob.jobUrl,
+    expiry: new Date(newJob.expires).toISOString().split("T")[0],
     // WEBINY CMS accepts a particular format for sending Dates
   };
   createJobResult.executeMutation(variables).then((result) => {
@@ -75,37 +77,31 @@ const addJob = () => {
   });
 };
 /** Reset all the dynamic fields after submitting the form */
-function resetFields() {
-  title.value = "";
-  jobUrl.value = "";
-  desc.value = "";
-  location.value = "";
-  contact.value = "";
-  expires.value = "";
+function resetFields(newJob) {
+  newJob.title = "";
+  newJob.jobUrl = "";
+  newJob.desc = "";
+  newJob.location = "";
+  newJob.contact = "";
+  newJob.expires = "";
 }
 </script>
 <template>
   <h1>CREATE JOB</h1>
   <!-- Form for creating a job. This will push entered values into the CMS -->
-  <form class="jobForm" @submit.prevent="checkFields()">
+  <form class="jobForm" @submit.prevent="checkFields(newJob)">
     <label for="title">Title</label>
-    <input type="text" name="title" v-model="title" />
+    <input type="text" name="title" v-model="newJob.title" />
     <label for="desc">Job Description</label>
-    <textarea name="desc" class="jobDesc" rows="5" v-model="desc"> </textarea>
+    <textarea name="desc" class="jobDesc" rows="5" v-model="newJob.desc"> </textarea>
     <label for="URL">Job Reference URL</label>
-    <input
-      type="url"
-      name="URL"
-      placeholder="http://"
-      pattern="http://.*"
-      v-model="jobUrl"
-    />
+    <input type="url" name="URL" placeholder="http://" pattern="http://.*" v-model="newJob.jobUrl" />
     <label for="contact">Job Contact Email</label>
-    <input type="email" name="contact" v-model="contact" />
+    <input type="email" name="contact" v-model="newJob.contact" />
     <label for="location">Job Location</label>
-    <input type="text" name="location" v-model="location" />
+    <input type="text" name="location" v-model="newJob.location" />
     <label for="expiry">Job Start Date</label>
-    <input type="date" v-model="expires" name="expiry" />
+    <input type="date" v-model="newJob.expires" name="expiry" />
     <button type="submit" value="Submit">ADD JOB</button>
   </form>
 </template>
